4f08a9df6b514eefe04d04762b96ebecf77d0ca7
Raj Mishra <raj212558@gmail.com>
2026-07-05 12:53:31 +0530

    update app.ts with rateLimiter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,15 @@ import express from "express";
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
+// set RateLimit
+app.use(rateLimiter);
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 import healthzRouter from "./routes/healthz/healthz.route";
+import { rateLimiter } from "./middleware/rateLimiter";
 
 app.use("/api/v1/", healthzRouter);
 
