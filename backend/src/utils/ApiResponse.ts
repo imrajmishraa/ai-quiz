700a1a11eeaffdd56a38f63b3a293d8e8a35ea4a
Raj Mishra <raj212558@gmail.com>
2026-07-06 17:35:28 +0530

    fix: typo error

--- backend/src/utils/ApiResponse.ts
+++ backend/src/utils/ApiResponse.ts
@@ -1,12 +1,12 @@
 class ApiResponse<T> {
   public readonly statusCode: number;
-  public readonly sucess: boolean;
+  public readonly success: boolean;
   public readonly message: string;
   public readonly data: T;
 
   constructor(statusCode: number, data: T, message: string = "success") {
     this.statusCode = statusCode;
-    this.sucess = statusCode >= 200 && statusCode <= 400;
+    this.success = statusCode >= 200 && statusCode <= 400;
     this.message = message;
     this.data = data;
   }
